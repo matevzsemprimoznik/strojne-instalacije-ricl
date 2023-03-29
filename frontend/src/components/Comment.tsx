a2e11432076c73e2848fdc22994665bc576a056f
Matevz <semprimoznik.matevz@gmail.com>
2023-03-29 20:07:47 +0200

    feat: add footer

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -11,7 +11,7 @@ const Comment: FC<CommentProps> = ({ date, comment, author }) => {
         <div className='flex flex-col text-center items-center rounded-3xl border-8 border-myLightBlue shadow-xl shadow-myLightBlue px-12 py-8 w-1/4 gap-y-4 mr-12'>
             <p className='font-bold text-myDarkGrey'>{date}</p>
             <p className='font-bold'>{comment}</p>
-            <p className='font-bold text-myBlue'>{author}</p>
+            <p className='font-bold text-custom-blue'>{author}</p>
         </div>
     )
 }
