9bd5147b930c4c49ec2087b00fd086790a12c035
jernej10 <jernej.ofic@gmail.com>
2023-04-05 14:02:25 +0200

    Fix: minor fixes

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import {formatDate} from "@/utils/date";
 
 type CommentProps = {
     date: string;
@@ -10,7 +11,7 @@ const Comment: FC<CommentProps> = ({ date, comment, author }) => {
     return (
         <div className='sm:px-2'>
             <div className='flex flex-col text-center items-center rounded-3xl border-8 border-myLightBlue shadow-xl shadow-myLightBlue px-5 sm:px-8 py-8'>
-                <p className='font-bold text-myDarkGrey'>{date}</p>
+                <p className='font-bold text-myDarkGrey'>{formatDate(date)}</p>
                 <p className='font-bold my-3'>{comment}</p>
                 <p className='font-bold text-custom-blue'>{author}</p>
             </div>
--- frontend/src/components/home/CounterSection.tsx
+++ frontend/src/components/home/CounterSection.tsx
@@ -6,33 +6,39 @@ import CountUp from 'react-countup';
 
 const CounterSection = () => {
     return (
-        <div className='flex flex-col justify-center items-center -mt-12 my-16 px-6 md:px-0 md:mx-auto md:w-2/3 lg:w-1/2'>
-            <div className='flex flex-col md:flex-row justify-between items-center w-full'>
-                <div className='text-center md:text-left mb-5 md:mb-0'>
-                    <p className='font-bold text-5xl text-myBlue'>
-                        <CountUp end={65} />
-                        <span className='text-black'>+</span>
-                    </p>
-                    <p className='font-bold text-2xl text-myDarkGrey'>zadovoljnih uporabnikov</p>
-                </div>
-                <div className='border-l-4 hidden md:block border-myLightBlue mx-4 h-24'></div>
-                <div className='text-center md:text-left mb-5 md:mb-0'>
-                    <p className='font-bold text-5xl text-myBlue'>
-                        <CountUp end={89} />
-                        <span className='text-black'>+</span>
-                    </p>
-                    <p className='font-bold text-2xl text-myDarkGrey'>uspešnih projektov</p>
-                </div>
-                <div className='border-l-4 hidden md:block border-myLightBlue h-24 mx-4'></div>
-                <div className='text-center md:text-left mb-5 md:mb-0'>
-                    <p className='font-bold text-5xl text-myBlue'>
-                        <CountUp end={8} />
-                        <span className='text-black'>+</span>
-                    </p>
-                    <p className='font-bold text-2xl text-myDarkGrey'>let izkušenj</p>
+        <>
+            <div className='flex items-center w-full flex-col'>
+                <h3 className='text-center text-xl font-bold text-custom-blue mb-3 z-10'>Naše delo</h3>
+                <h2 className='text-center text-4xl font-bold text tracking-wider z-10'>Kakovostno delo nam <br/> je prineslo</h2>
+            </div>
+            <div className='flex flex-col justify-center items-center my-16 px-6 md:px-0 md:mx-auto md:w-2/3 lg:w-1/2'>
+                <div className='flex flex-col md:flex-row justify-between items-center w-full'>
+                    <div className='text-center md:text-left mb-5 md:mb-0'>
+                        <p className='font-bold text-5xl text-myBlue'>
+                            <CountUp enableScrollSpy end={65} />
+                            <span className='text-black'>+</span>
+                        </p>
+                        <p className='font-bold text-2xl text-myDarkGrey'>zadovoljnih uporabnikov</p>
+                    </div>
+                    <div className='border-l-4 hidden md:block border-myLightBlue mx-4 h-24'></div>
+                    <div className='text-center md:text-left mb-5 md:mb-0'>
+                        <p className='font-bold text-5xl text-myBlue'>
+                            <CountUp enableScrollSpy end={89} />
+                            <span className='text-black'>+</span>
+                        </p>
+                        <p className='font-bold text-2xl text-myDarkGrey'>uspešnih projektov</p>
+                    </div>
+                    <div className='border-l-4 hidden md:block border-myLightBlue h-24 mx-4'></div>
+                    <div className='text-center md:text-left mb-5 md:mb-0'>
+                        <p className='font-bold text-5xl text-myBlue'>
+                            <CountUp enableScrollSpy end={8} />
+                            <span className='text-black'>+</span>
+                        </p>
+                        <p className='font-bold text-2xl text-myDarkGrey'>let izkušenj</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
