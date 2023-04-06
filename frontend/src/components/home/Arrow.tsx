d0a6086f78ea9abb0419b339288494afecad4505
Matevz <semprimoznik.matevz@gmail.com>
2023-04-06 20:38:45 +0200

    fix: hide arrow on mobile

--- frontend/src/components/home/Arrow.tsx
+++ frontend/src/components/home/Arrow.tsx
@@ -1,10 +1,11 @@
 import {twMerge} from "tailwind-merge";
 
 const Arrow = (props: any) => {
-    const {className, style, onClick} = props;
+    const {className, onClick} = props;
+    const disabledClass = className.toString().includes('disabled') ? 'slick-disabled' : ''
     const rotate = props.direction === 'left' ? 'rotate(135deg)' : 'rotate(-45deg)'
     const position = props.direction === 'left' ? 'left-[-14px] lg:left-[-20px]' : 'right-[-14px] lg:right-[-20px]'
-    return <div style={{transform: rotate}} onClick={onClick} className={twMerge(className, position, `arrow before:content-none hidden sm:block`)}></div>
+    return <div style={{transform: rotate}} onClick={onClick} className={twMerge(disabledClass, position, `arrow before:content-none hidden lg:block`)}></div>
 }
 
 export default Arrow
