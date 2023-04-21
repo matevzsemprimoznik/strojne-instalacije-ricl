86b6147567358b0ae1b38140be7f46fba60461a8
Matevz <semprimoznik.matevz@gmail.com>
2023-04-21 13:14:05 +0200

    fix: middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,28 +6,23 @@ import acceptLanguage from "accept-language";
 
 const cookieName = 'i18next'
 
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)']
+}
+
 export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname
-
-    if (pathname.indexOf('icon') > -1 || pathname.indexOf('chrome') > -1)
-        return NextResponse.next()
-
-    let lng:string | null = null
-
-    const languageCookie = request.cookies.get(cookieName)
-    if (languageCookie)
-        lng = acceptLanguage.get(languageCookie.value)
-    if (!lng)
-        lng = acceptLanguage.get(request.headers.get('Accept-Language'))
+    let lng
+    if (request.cookies.has(cookieName)) lng = acceptLanguage.get(request.cookies.get(cookieName)?.value)
+    if (!lng) lng = acceptLanguage.get(request.headers.get('Accept-Language'))
     if (!lng) lng = i18n.defaultLocale
 
+    if (request.nextUrl.pathname === '/') {
+        return NextResponse.redirect(new URL(`/${lng}`, request.url))
+    }
 
-    if (!i18n.locales.some(loc => pathname.startsWith(`/${loc}`)) && !pathname.startsWith('/_next'))
-        return NextResponse.redirect(new URL(`/${lng}${pathname}`, request.url))
-
-    const refererHeader = request.headers.get('referer')
-    if (refererHeader) {
-        const refererUrl = new URL(refererHeader)
+    const referer = request.headers.get('referer')
+    if (referer) {
+        const refererUrl = new URL(referer)
         const lngInReferer = i18n.locales.find((l) => refererUrl.pathname.startsWith(`/${l}`))
         const response = NextResponse.next()
         if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
@@ -37,6 +32,6 @@ export function middleware(request: NextRequest) {
     return NextResponse.next()
 }
 
-export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|robots.txt|sitemap.xml).*)']
-}
+// export const config = {
+//     matcher: ['/','/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|robots.txt|sitemap.xml).*)']
+// }
