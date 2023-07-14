21424392e8169fe0b7efc832dedddf87162b159b
Matevz <semprimoznik.matevz@gmail.com>
2023-07-14 18:50:31 +0200

    feat: add local business structured data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import {ReactNode} from "react";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Providers from "@/app/providers";
+import Script from "next/script";
+import {localBusinessData} from "@/assets/local-business-data";
 
 interface RootLayoutProps {
     children: ReactNode,
@@ -10,11 +12,18 @@ interface RootLayoutProps {
 const RootLayout = async ({children}: RootLayoutProps) => {
     return (
         <html>
-            <head>
-                <link rel="icon" href="favicon.ico"/>
-                <meta name="google-site-verification" content="UQsgnJIYQYh3d4kIIz6SsTNisf9SYvieEqmcMTJZAfk"/>
-            </head>
-            <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA4_TRACKING_ID || ''}/>
+        <head>
+            <link rel="icon" href="favicon.ico"/>
+            <meta name="google-site-verification" content="UQsgnJIYQYh3d4kIIz6SsTNisf9SYvieEqmcMTJZAfk"/>
+            <Script
+                id="app-ld-json"
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify(localBusinessData, null, "\t"),
+                }}
+            />
+        </head>
+        <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA4_TRACKING_ID || ''}/>
             <body>
             <Providers>{children}</Providers>
             </body>
---
+++ src/assets/local-business-data.ts
@@ -0,0 +1,24 @@
+export const localBusinessData = {
+    "@context": "https://schema.org",
+    "@type": "ProfessionalService",
+    "name": "Strojne inštalacije Ričl",
+    "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Kremberk 47 B",
+        "addressLocality": "Sveta Ana v Slovenskih goricah",
+        "postalCode": "2233",
+        "addressCountry": "SI"
+    },
+    "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": 46.645502,
+        "longitude": 15.837802
+    },
+    "url": "https://strojne-instalacije-ricl.vercel.app/domov",
+    "telephone": "+386 41 508744",
+    "email": "davorinricl@gmail.com",
+    "image": [
+        "https://strojne-instalacije-ricl.vercel.app/local-business-data-image1.png",
+        "https://strojne-instalacije-ricl.vercel.app/local-business-data-image2.png",
+    ]
+}
